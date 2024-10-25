52cc4c9aa350bb6af9d539cc2f60e8fdf1a0b6e9
ABNext <alfredobrito@hotmail.com>
2024-10-25 01:28:15 -0500

    url commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
+  async rewrites() {
     return [
       {
         source: '/api/:path*',
         destination: 'https://201.183.9.59:9002/api/:path*',
       },
-    ]
+    ];
   },
 }
 
