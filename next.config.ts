8ac8a9eb8e75d66d1d3b6dda6354b60d24299d29
ABNext <alfredobrito@hotmail.com>
2024-11-01 18:05:47 -0500

    part2y2 commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['177.234.196.99'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '177.234.196.99',
+        port: '8089',
+        pathname: '/images/**',
+      },
+    ],
   },
   async rewrites() {
     return [
